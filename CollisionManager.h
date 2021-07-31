a69ffb1079e6cfb2f1f28af761b89cdedaa3e224
Eric Huang <erichuangg99@gmail.com>
2021-07-30 18:30:09 -0700

    Best state trained

--- CollisionManager.h
+++ CollisionManager.h
@@ -22,7 +22,7 @@ public:
         if (sf::FloatRect(rr.left, rr.top-rr.height, rr.width, rr.top)
             .intersects(sf::FloatRect(fr.left, fr.top-fr.height, fr.width, fr.height)) || 
             rr.top - rr.height < fr.top) {
-            if ((r.getRotation() < 2 || r.getRotation() > 358) && abs(r.getVelocity().y) < 3 && abs(r.getVelocity().x) < 2) {
+            if ((r.getRotation() < 2 || r.getRotation() > 358) && abs(r.getVelocity().y) < 3 && abs(r.getVelocity().x) < 3) {
                 return Rocket::Status::Landed;
             } 
             return Rocket::Status::Explode;
--- manager.h
+++ manager.h
@@ -54,10 +54,10 @@ class Manager {
             }
 #if defined(CPU)
             // ai_manager.init_random(rockets);
-            ai_manager.init_from_file(rockets, "C:\\Users\\Eric\\ProgrammingProjectsCpp\\RocketSaves\\V2Run1\\iteration300.txt");
+            ai_manager.init_from_file(rockets, "C:\\Users\\Eric\\ProgrammingProjectsCpp\\RocketSaves\\V2Run1\\iteration3000.txt");
 #elif defined(GPU)
             // ai_manager.init_random(rockets);
-            ai_manager.init_from_file(rockets, "../saves/goodsaves/iteration300.txt");
+            ai_manager.init_from_file(rockets, "../saves/goodsaves/iteration3300.txt");
 #endif
         }
         ~Manager() 
