58b323c23ee6a30bdbc1d005c7839b708cde64a4
Eric Huang <erichuangg99@gmail.com>
2021-08-01 20:23:00 -0700

    m

--- Env.h
+++ Env.h
@@ -103,8 +103,8 @@ sf::Clock Env::clock{};
 sf::Vector2f Env::origin(Env::ww/2, Env::wh/2); // (position (0, 0))
 sf::Vector2f Env::camera_pos(0, 0);
 int Env::cycle_num = 0;
-int Env::num_rocks = 300;
-int Env::load_rocks = 300;
+int Env::num_rocks = 250;
+int Env::load_rocks = 250;
 
 std::random_device Env::seed;
 std::mt19937 Env::rng(Env::seed());
--- manager.h
+++ manager.h
@@ -54,10 +54,10 @@ class Manager {
             }
 #if defined(CPU)
             // ai_manager.init_random(rockets);
-            ai_manager.init_from_file(rockets, "C:\\Users\\Eric\\ProgrammingProjectsCpp\\RocketSaves\\V2Run1\\iteration1050.txt");
+            ai_manager.init_from_file(rockets, "C:\\Users\\Eric\\ProgrammingProjectsCpp\\RocketSaves\\V2Run1\\iteration2850.txt");
 #elif defined(GPU)
             ai_manager.init_random(rockets);
-            // ai_manager.init_from_file(rockets, "../saves/pres3/iteration480.txt");
+            // ai_manager.init_from_file(rockets, "../saves/pres1/iteration9300.txt");
 #endif
         }
         ~Manager() 
